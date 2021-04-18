c6c1d9bc05820469d0f1473b1cb967d792ef5cb4
Shashank Dixit <shashanksdixit@gmail.com>
2021-04-18 18:00:39 +0530

    case02

--- reactorproject/src/main/java/monocreate/Case02MonoSubscribe.java
+++ reactorproject/src/main/java/monocreate/Case02MonoSubscribe.java
@@ -4,9 +4,20 @@ import reactor.core.publisher.Mono;
 
 public class Case02MonoSubscribe {
     public static void main(String[] args) {
+        // Mono with subscription consumers
+        System.out.println("------- Mono - Subscription Consumers --------");
+
         Mono<Integer> mono = Mono.just(10);
         mono.subscribe(i -> System.out.println("Received : " + i),
                 err -> System.out.println(err.getMessage()),
                 () -> System.out.println("Completed."));
+
+        //Mono - error example
+        System.out.println("------- Mono - Error example --------");
+        Mono.just(10)
+                .map(i -> i / 0)
+                .subscribe(i -> System.out.println("Received : " + i),
+                        err -> System.out.println(err.getMessage()),
+                        () -> System.out.println("Completed."));
     }
 }
