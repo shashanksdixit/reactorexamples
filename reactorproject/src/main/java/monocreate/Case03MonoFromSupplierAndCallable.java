b99598d00e50788ca8d064daa185009530231511
Shashank Dixit <shashanksdixit@gmail.com>
2022-02-08 12:39:38 +0530

    updated the example

--- reactorproject/src/main/java/monocreate/Case03MonoFromSupplierAndCallable.java
+++ reactorproject/src/main/java/monocreate/Case03MonoFromSupplierAndCallable.java
@@ -3,6 +3,8 @@ package monocreate;
 import com.github.javafaker.Faker;
 import reactor.core.publisher.Mono;
 
+import java.util.stream.Stream;
+
 public class Case03MonoFromSupplierAndCallable {
     public static void main(String[] args) {
         System.out.println("------ From Supplier ----");
@@ -12,5 +14,24 @@ public class Case03MonoFromSupplierAndCallable {
         System.out.println("------ From Supplier ----");
         Mono<String> fromCallable = Mono.fromCallable(() -> Faker.instance().name().fullName());
         fromCallable.subscribe(System.out::println);
+
+        System.out.println("------ Example ---------");
+        Stream.of(1, 2, 0)
+                        .forEach(id -> currencyRepository(id)
+                .subscribe(currency -> System.out.println("Currency " + currency),
+                        err -> System.out.println("Error " + err.toString()),
+                        () -> System.out.println("..Complete.."))
+        );
+    }
+
+    private static Mono<String> currencyRepository(int id) {
+        if (id == 1) {
+            return Mono.fromSupplier(() -> Faker.instance().currency().name());
+        } else if (id == 2) {
+            return Mono.empty();
+        } else if (id == 0) {
+            return Mono.error(new RuntimeException("Not allowed"));
+        }
+        return Mono.fromSupplier(() -> Faker.instance().currency().name());
     }
 }
