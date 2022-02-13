e3cade84b85326efe29dfdbc51d987ea7a3eaece
Shashank Dixit <shashanksdixit@gmail.com>
2022-02-13 09:47:33 +0530

    added flatmap concatmap

--- reactorproject/src/main/java/fluxcreate/Case02FluxMultipleSubscribers.java
+++ reactorproject/src/main/java/fluxcreate/Case02FluxMultipleSubscribers.java
@@ -1,16 +1,26 @@
 package fluxcreate;
 
 import reactor.core.publisher.Flux;
+import reactor.util.Logger;
+
+import java.time.Duration;
+import java.util.logging.Level;
 
 public class Case02FluxMultipleSubscribers {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Flux<Integer> intFlux = Flux.just(1, 2, 3, 4, 5, 6);
 
-        intFlux.subscribe(i -> System.out.println("Subscriber 1 : " + i));
+        intFlux
+                .log()
+                .map(i -> i * 10)
+                .log()
+                .delayElements(Duration.ofMillis(1))
+                .log()
+                .subscribe(i -> System.out.println("Subscriber 1 : " + i));
 
-        intFlux.subscribe(i -> System.out.println("Subscriber 2 : " + i));
+//        intFlux.subscribe(i -> System.out.println("Subscriber 2 : " + i));
 
-        intFlux.filter(i -> i % 2 == 0).subscribe(i -> System.out.println("Subscriber 3 : " + i));
-
+//        intFlux.filter(i -> i % 2 == 0).log().subscribe(i -> System.out.println("Subscriber 3 : " + i));
+        Thread.sleep(10000);
     }
 }
