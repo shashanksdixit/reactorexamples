655f01e9592c1f93e7537315ed08c43c27800afe
Shashank Dixit <shashanksdixit@gmail.com>
2022-02-21 15:17:22 +0530

    added logging

--- reactorproject/src/main/java/fluxcreate/Case01FluxJust.java
+++ reactorproject/src/main/java/fluxcreate/Case01FluxJust.java
@@ -1,10 +1,13 @@
 package fluxcreate;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 public class Case01FluxJust {
     public static void main(String[] args) {
+        log.debug("Starting program");
         Flux<Integer> integerFlux = Flux.just(1, 2, 3, 4);
-        integerFlux.subscribe(System.out::println);
+        integerFlux.log().subscribe(System.out::println);
     }
 }
