6e86a55bb6b7d00a9b9d1551a54177331ef0b2ae
Shashank Dixit <shashanksdixit@gmail.com>
2022-03-01 15:55:11 +0530

    modified

--- reactorproject/src/main/java/fluxcreate/Case10FluxCreateCheckCancel.java
+++ reactorproject/src/main/java/fluxcreate/Case10FluxCreateCheckCancel.java
@@ -3,14 +3,14 @@ package fluxcreate;
 import com.github.javafaker.Faker;
 import reactor.core.publisher.Flux;
 
-public class Case10FluxCreateTakeOperator {
+public class Case10FluxCreateCheckCancel {
     public static void main(String[] args) {
-        // Check how flux sink emits even after onComplete. Now uncomment the code and check.
+        // Check how flux sink emits even after onComplete. Now uncomment the if statement and check.
         Flux.create(fluxSink -> {
             for (int i = 0; i < 10; i++) {
                 String airport = Faker.instance().aviation().airport();
-/*                if (fluxSink.isCancelled())
-                    return; */
+                if (fluxSink.isCancelled()) // After "take(n)" flux sink should stop
+                    return;
                 System.out.println("Emitting : " + airport);
                 fluxSink.next(airport);
             }
